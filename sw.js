ec02bf74433681bf825ba84b7035b75ac3ea6fa0
steve210305-tech <steve210305@gmail.com>
2026-09-22 19:45:12 +0200

    Refresh PWA cache for 3.2.0

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION='3.1.1';
+const VERSION='3.2.0';
 const CACHE=`4w1f-${VERSION}`;
 const SHELL=['./','./index.html','./production.js','./manifest.webmanifest','./version.json','./assets/icon-180.png','./assets/icon-192.png','./assets/icon-512.png','./assets/favicon-32.png'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(SHELL)).catch(()=>{}))});
